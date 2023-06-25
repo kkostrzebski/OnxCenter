ca7dc007633a2647d55034572c23cc2f5ce520e6
Konrad <ko.kostrzebski@gmail.com>
2023-06-25 21:09:16 +0200

    metoda "make" zwraca funkcje, ktora przyjmuje jeden parametr

--- PHP/zadanie1.php
+++ PHP/zadanie1.php
@@ -3,7 +3,13 @@
 class Pipeline {
 
     public static function make(...$functions) {
-        
+        return function ($arg) use ($functions) {
+            $result = $arg;
+            foreach ($functions as $function) {
+                $result = $function($result);
+            }
+            return $result;
+        };
     }
   
   };
