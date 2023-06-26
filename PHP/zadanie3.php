224bd362c93b0bef273b174f92bb490278399ee4
Konrad <ko.kostrzebski@gmail.com>
2023-06-26 08:34:43 +0200

    stworzono strukture dla kazdego gracza

--- PHP/zadanie3.php
+++ PHP/zadanie3.php
@@ -1,8 +1,18 @@
 <?php
 
 
-
-
-
+class RankingTable {
+    private $players = [];
+  
+    public function __construct($playerNames) {
+      foreach ($playerNames as $name) {
+        $this->players[$name] = [
+          'score' => 0,
+          'gamesPlayed' => 0
+        ];
+      }
+    }
+  
+}
 
 ?>
