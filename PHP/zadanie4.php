bf75cf55ed0e399370c2d85682d3780ed388e095
Konrad <ko.kostrzebski@gmail.com>
2023-06-26 11:16:03 +0200

    stworzono strukture danych jako zmienna globalna

---
+++ PHP/zadanie4.php
@@ -0,0 +1,14 @@
+<?php
+
+$thesaurus = array(
+    "market" => array("trade"),
+    "small" => array("little", "compact")
+  );
+
+  function getSynonyms($word) {
+    global $thesaurus;
+
+    
+}
+
+?>
