79be54175f7d2d91f6340c9bcf92f967547df7ba
Konrad <ko.kostrzebski@gmail.com>
2023-06-26 10:10:24 +0200

    sprawdzenie poprawnosci wykonania

--- PHP/zadanie3.php
+++ PHP/zadanie3.php
@@ -30,4 +30,10 @@ class RankingTable {
    
 }
 
+$table = new RankingTable(array('Jan', 'Maks', 'Monika'));
+$table->recordResult('Jan', 2);
+$table->recordResult('Maks', 3);
+$table->recordResult('Monika', 5);
+echo $table->playerRank(1);
+
 ?>
